36ce769556545df96d44d9d8ff08855808ac4eda
aLEKS-e3 <chebukin404@gmail.com>
2024-03-08 15:08:46 +0200

    Modified settings - db, secret key - .gitignore, and requirements.txt

--- ooh_notes/settings.py
+++ ooh_notes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from os import path
 from pathlib import Path
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o+&p8x@$%$3m0r!^0kc8$p4jaj#)w=9b91e$kz$i2cus+b@ou8"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-o+&p8x@$%$3m0r!^0kc8$p4jaj#)w=9b91e$kz$i2cus+b@ou8")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
