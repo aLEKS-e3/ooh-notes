f6e223bed974fdbbfa4f9d4edceb5c6e3ff43552
aLEKS-e3 <chebukin404@gmail.com>
2024-03-08 15:46:39 +0200

    Add production db settings + update requirements.txt

--- ooh_notes/settings.py
+++ ooh_notes/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+import dj_database_url
 from os import path
 from pathlib import Path
 
@@ -92,6 +93,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
