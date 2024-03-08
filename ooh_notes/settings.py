a5475c6a214a419415995851a2fcdc742689d203
aLEKS-e3 <chebukin404@gmail.com>
2024-03-08 16:38:03 +0200

    Add allowed hosts

--- ooh_notes/settings.py
+++ ooh_notes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "https://ooh-notes.onrender.com"]
 
 # Application definition
 
